bfd59358dd467e88e2240daeca45453a507a689d
limon <limonpaulargo47@gmail.com>
2023-11-06 10:47:21 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,167 @@
+import 'package:test/test.dart';
+
+void main() {
+/*  print('Hello world');
+  print('Hello world');
+// add tow nuber
+  var num1 = 10;
+  var num2 = 20;
+  var num = num1 + num2;
+  print(num);
+
+  //most important five data types 1.Number 2.String 3.Boolean 4.Lists 5.Maps
+//use string
+  var myCountry = 'My Country name is Bangladesh';
+  print(myCountry);
+
+  //use boolean
+  var isBangladeshi = true;
+  print(isBangladeshi);
+
+  //use List
+  var productItem = ['cloth', 'cosmatic', 'shari', 'Show', 'vegetables'];
+
+  print(productItem.length);
+
+  //use map
+
+  var person = {'name': 'Limon', 'age': '22', 'hight': '5 Feet  6 Inches'};
+  print(person);
+  print(person['hight']);
+  print(person['age']);
+  print(person['name']);
+
+  //arithmatic operator
+
+  var a = 20;
+  var b = 20;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+
+  //use unary operator
+  var x = 10;
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+
+  var y = 10;
+  print(--y);
+  print(--y);
+  print(y--);
+  print(y--);
+
+  //use final and costant
+  final thisYear = 2005;
+
+  print(thisYear);
+
+  /* List properties
+  first,
+  last,
+  isEmpty,
+  isNotEmpty,
+  Length,
+  reverced,
+  single,*/
+  var city = <String>[
+    'Dhaka',
+    'Rangpur',
+    'Rajshahi',
+    'Bogura',
+  ];
+  city.add('Moulvibazar');
+  city.removeAt(2);
+
+  var result = city;
+
+  print(result[0]);
+
+  //Fixed LengthList
+
+  // * The fixed length lists are deffined with the specified length
+
+  var citys = ['MoulviBazar', 'Bogura', 'Rajshahi', 'Maymensing', 'Jamalpur'];
+  print(citys);
+  citys.add('Joydebpur');
+
+  print(citys);
+
+  print(citys.length);
+  citys.insert(0, 'Maijpara');
+  print(citys);
+  citys.insertAll(5, ['india', 'Bangladesh']);
+  print(citys);
+
+  citys[1] = 'kashmir';
+  citys[2] = 'Netrokona';
+
+  print(citys);
+  citys.removeLast();
+
+  print(citys);
+  citys.removeAt(0);
+  print(citys);
+  citys.remove('kashmir');
+  print(citys);*/
+
+//Live class-2
+  /// dock comment.
+
+  /* print('HELLO WORLD');
+  print('Hello from ostad');
+  print(26526);
+  print(4.57);
+  print(545 + 45);//add
+  print(34 - 34);//sub
+  print(34 / 34);//divission
+  print(34 * 4);//multyply
+  print(34 % 3);//modulus*/
+
+  // variable
+
+// variable holo container.
+
+  String firstName = 'Rahim';
+  String lastName = 'karim';
+  //string concatination
+  String fullName = '$firstName $lastName';
+  print(fullName);
+  int age = 24;
+  double cgpa = 4.50;
+  bool dinnerdone = true;
+  age = 45;
+  cgpa = 3.70;
+
+  //string interpulation
+  String details = 'FullName:$fullName\nAge:$age\ncgpa:$cgpa';
+  print(details);
+
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(cgpa);
+  print(dinnerdone);
+
+  int a = 65;
+  int b = 56;
+  int c = a + b;
+  print(c);
+
+  double g = 3.1416;
+  double t = 2.5;
+  double h = g + t;
+  print(h);
+
+  double k = 2.5;
+  double j = 2.5;
+  double m = k + j;
+  print(m);
+}
